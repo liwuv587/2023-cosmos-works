5a96702abd242cccaaa6fa9341dcecd80303fb2a
liwuv587 <liwuv587@gmail.com>
2023-06-28 20:59:47 +0800

    work4 work: step 1 创建  updatePost。
     ignite scaffold packet updatePost postID title content --ack postID title --module blog

---
+++ week4/planet/x/blog/keeper/update_post.go
@@ -0,0 +1,82 @@
+package keeper
+
+import (
+	"errors"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	clienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
+	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
+	host "github.com/cosmos/ibc-go/v7/modules/core/24-host"
+	"planet/x/blog/types"
+)
+
+// TransmitUpdatePostPacket transmits the packet over IBC with the specified source port and source channel
+func (k Keeper) TransmitUpdatePostPacket(
+	ctx sdk.Context,
+	packetData types.UpdatePostPacketData,
+	sourcePort,
+	sourceChannel string,
+	timeoutHeight clienttypes.Height,
+	timeoutTimestamp uint64,
+) (uint64, error) {
+	channelCap, ok := k.scopedKeeper.GetCapability(ctx, host.ChannelCapabilityPath(sourcePort, sourceChannel))
+	if !ok {
+		return 0, sdkerrors.Wrap(channeltypes.ErrChannelCapabilityNotFound, "module does not own channel capability")
+	}
+
+	packetBytes, err := packetData.GetBytes()
+	if err != nil {
+		return 0, sdkerrors.Wrapf(sdkerrors.ErrJSONMarshal, "cannot marshal the packet: %w", err)
+	}
+
+	return k.channelKeeper.SendPacket(ctx, channelCap, sourcePort, sourceChannel, timeoutHeight, timeoutTimestamp, packetBytes)
+}
+
+// OnRecvUpdatePostPacket processes packet reception
+func (k Keeper) OnRecvUpdatePostPacket(ctx sdk.Context, packet channeltypes.Packet, data types.UpdatePostPacketData) (packetAck types.UpdatePostPacketAck, err error) {
+	// validate packet data upon receiving
+	if err := data.ValidateBasic(); err != nil {
+		return packetAck, err
+	}
+
+	// TODO: packet reception logic
+
+	return packetAck, nil
+}
+
+// OnAcknowledgementUpdatePostPacket responds to the the success or failure of a packet
+// acknowledgement written on the receiving chain.
+func (k Keeper) OnAcknowledgementUpdatePostPacket(ctx sdk.Context, packet channeltypes.Packet, data types.UpdatePostPacketData, ack channeltypes.Acknowledgement) error {
+	switch dispatchedAck := ack.Response.(type) {
+	case *channeltypes.Acknowledgement_Error:
+
+		// TODO: failed acknowledgement logic
+		_ = dispatchedAck.Error
+
+		return nil
+	case *channeltypes.Acknowledgement_Result:
+		// Decode the packet acknowledgment
+		var packetAck types.UpdatePostPacketAck
+
+		if err := types.ModuleCdc.UnmarshalJSON(dispatchedAck.Result, &packetAck); err != nil {
+			// The counter-party module doesn't implement the correct acknowledgment format
+			return errors.New("cannot unmarshal acknowledgment")
+		}
+
+		// TODO: successful acknowledgement logic
+
+		return nil
+	default:
+		// The counter-party module doesn't implement the correct acknowledgment format
+		return errors.New("invalid acknowledgment format")
+	}
+}
+
+// OnTimeoutUpdatePostPacket responds to the case where a packet has not been transmitted because of a timeout
+func (k Keeper) OnTimeoutUpdatePostPacket(ctx sdk.Context, packet channeltypes.Packet, data types.UpdatePostPacketData) error {
+
+	// TODO: packet timeout logic
+
+	return nil
+}
--- week4/planet/x/blog/types/packet.pb.go
+++ week4/planet/x/blog/types/packet.pb.go
@@ -26,6 +26,7 @@ type BlogPacketData struct {
 	// Types that are valid to be assigned to Packet:
 	//	*BlogPacketData_NoData
 	//	*BlogPacketData_IbcPostPacket
+	//	*BlogPacketData_UpdatePostPacket
 	Packet isBlogPacketData_Packet `protobuf_oneof:"packet"`
 }
 
@@ -74,9 +75,13 @@ type BlogPacketData_NoData struct {
 type BlogPacketData_IbcPostPacket struct {
 	IbcPostPacket *IbcPostPacketData `protobuf:"bytes,2,opt,name=ibcPostPacket,proto3,oneof" json:"ibcPostPacket,omitempty"`
 }
+type BlogPacketData_UpdatePostPacket struct {
+	UpdatePostPacket *UpdatePostPacketData `protobuf:"bytes,3,opt,name=updatePostPacket,proto3,oneof" json:"updatePostPacket,omitempty"`
+}
 
-func (*BlogPacketData_NoData) isBlogPacketData_Packet()        {}
-func (*BlogPacketData_IbcPostPacket) isBlogPacketData_Packet() {}
+func (*BlogPacketData_NoData) isBlogPacketData_Packet()           {}
+func (*BlogPacketData_IbcPostPacket) isBlogPacketData_Packet()    {}
+func (*BlogPacketData_UpdatePostPacket) isBlogPacketData_Packet() {}
 
 func (m *BlogPacketData) GetPacket() isBlogPacketData_Packet {
 	if m != nil {
@@ -99,11 +104,19 @@ func (m *BlogPacketData) GetIbcPostPacket() *IbcPostPacketData {
 	return nil
 }
 
+func (m *BlogPacketData) GetUpdatePostPacket() *UpdatePostPacketData {
+	if x, ok := m.GetPacket().(*BlogPacketData_UpdatePostPacket); ok {
+		return x.UpdatePostPacket
+	}
+	return nil
+}
+
 // XXX_OneofWrappers is for the internal use of the proto package.
 func (*BlogPacketData) XXX_OneofWrappers() []interface{} {
 	return []interface{}{
 		(*BlogPacketData_NoData)(nil),
 		(*BlogPacketData_IbcPostPacket)(nil),
+		(*BlogPacketData_UpdatePostPacket)(nil),
 	}
 }
 
@@ -249,34 +262,145 @@ func (m *IbcPostPacketAck) GetPostID() string {
 	return ""
 }
 
+// UpdatePostPacketData defines a struct for the packet payload
+type UpdatePostPacketData struct {
+	PostID  string `protobuf:"bytes,1,opt,name=postID,proto3" json:"postID,omitempty"`
+	Title   string `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
+	Content string `protobuf:"bytes,3,opt,name=content,proto3" json:"content,omitempty"`
+}
+
+func (m *UpdatePostPacketData) Reset()         { *m = UpdatePostPacketData{} }
+func (m *UpdatePostPacketData) String() string { return proto.CompactTextString(m) }
+func (*UpdatePostPacketData) ProtoMessage()    {}
+func (*UpdatePostPacketData) Descriptor() ([]byte, []int) {
+	return fileDescriptor_d544876606a92415, []int{4}
+}
+func (m *UpdatePostPacketData) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *UpdatePostPacketData) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_UpdatePostPacketData.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *UpdatePostPacketData) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_UpdatePostPacketData.Merge(m, src)
+}
+func (m *UpdatePostPacketData) XXX_Size() int {
+	return m.Size()
+}
+func (m *UpdatePostPacketData) XXX_DiscardUnknown() {
+	xxx_messageInfo_UpdatePostPacketData.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_UpdatePostPacketData proto.InternalMessageInfo
+
+func (m *UpdatePostPacketData) GetPostID() string {
+	if m != nil {
+		return m.PostID
+	}
+	return ""
+}
+
+func (m *UpdatePostPacketData) GetTitle() string {
+	if m != nil {
+		return m.Title
+	}
+	return ""
+}
+
+func (m *UpdatePostPacketData) GetContent() string {
+	if m != nil {
+		return m.Content
+	}
+	return ""
+}
+
+// UpdatePostPacketAck defines a struct for the packet acknowledgment
+type UpdatePostPacketAck struct {
+	PostID string `protobuf:"bytes,1,opt,name=postID,proto3" json:"postID,omitempty"`
+}
+
+func (m *UpdatePostPacketAck) Reset()         { *m = UpdatePostPacketAck{} }
+func (m *UpdatePostPacketAck) String() string { return proto.CompactTextString(m) }
+func (*UpdatePostPacketAck) ProtoMessage()    {}
+func (*UpdatePostPacketAck) Descriptor() ([]byte, []int) {
+	return fileDescriptor_d544876606a92415, []int{5}
+}
+func (m *UpdatePostPacketAck) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *UpdatePostPacketAck) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_UpdatePostPacketAck.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *UpdatePostPacketAck) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_UpdatePostPacketAck.Merge(m, src)
+}
+func (m *UpdatePostPacketAck) XXX_Size() int {
+	return m.Size()
+}
+func (m *UpdatePostPacketAck) XXX_DiscardUnknown() {
+	xxx_messageInfo_UpdatePostPacketAck.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_UpdatePostPacketAck proto.InternalMessageInfo
+
+func (m *UpdatePostPacketAck) GetPostID() string {
+	if m != nil {
+		return m.PostID
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*BlogPacketData)(nil), "planet.blog.BlogPacketData")
 	proto.RegisterType((*NoData)(nil), "planet.blog.NoData")
 	proto.RegisterType((*IbcPostPacketData)(nil), "planet.blog.IbcPostPacketData")
 	proto.RegisterType((*IbcPostPacketAck)(nil), "planet.blog.IbcPostPacketAck")
+	proto.RegisterType((*UpdatePostPacketData)(nil), "planet.blog.UpdatePostPacketData")
+	proto.RegisterType((*UpdatePostPacketAck)(nil), "planet.blog.UpdatePostPacketAck")
 }
 
 func init() { proto.RegisterFile("planet/blog/packet.proto", fileDescriptor_d544876606a92415) }
 
 var fileDescriptor_d544876606a92415 = []byte{
-	// 260 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x28, 0xc8, 0x49, 0xcc,
-	0x4b, 0x2d, 0xd1, 0x4f, 0xca, 0xc9, 0x4f, 0xd7, 0x2f, 0x48, 0x4c, 0xce, 0x4e, 0x2d, 0xd1, 0x2b,
-	0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x86, 0xc8, 0xe8, 0x81, 0x64, 0x94, 0x26, 0x32, 0x72, 0xf1,
-	0x39, 0xe5, 0xe4, 0xa7, 0x07, 0x80, 0x55, 0xb8, 0x24, 0x96, 0x24, 0x0a, 0xe9, 0x72, 0xb1, 0xe5,
-	0xe5, 0x83, 0x58, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0xdc, 0x46, 0xc2, 0x7a, 0x48, 0x1a, 0xf4, 0xfc,
-	0xc0, 0x52, 0x1e, 0x0c, 0x41, 0x50, 0x45, 0x42, 0x6e, 0x5c, 0xbc, 0x99, 0x49, 0xc9, 0x01, 0xf9,
-	0xc5, 0x25, 0x10, 0x33, 0x24, 0x98, 0xc0, 0xba, 0xe4, 0x50, 0x74, 0x79, 0x22, 0xab, 0x80, 0x1a,
-	0x80, 0xaa, 0xcd, 0x89, 0x83, 0x8b, 0x0d, 0xe2, 0x4c, 0x25, 0x0e, 0x2e, 0x36, 0x88, 0x2d, 0x4a,
-	0xb1, 0x5c, 0x82, 0x18, 0x3a, 0x85, 0x44, 0xb8, 0x58, 0x4b, 0x32, 0x4b, 0x72, 0x52, 0xc1, 0xce,
-	0xe3, 0x0c, 0x82, 0x70, 0x84, 0x24, 0xb8, 0xd8, 0x93, 0xf3, 0xf3, 0x4a, 0x52, 0xf3, 0x20, 0x0e,
-	0xe0, 0x0c, 0x82, 0x71, 0xc1, 0x32, 0x45, 0xa9, 0x89, 0x25, 0xf9, 0x45, 0x12, 0xcc, 0x50, 0x19,
-	0x08, 0x57, 0x49, 0x8b, 0x4b, 0x00, 0xc5, 0x78, 0xc7, 0xe4, 0x6c, 0x21, 0x31, 0x2e, 0xb6, 0x82,
-	0xfc, 0xe2, 0x12, 0x4f, 0x17, 0xa8, 0xf1, 0x50, 0x9e, 0x93, 0xee, 0x89, 0x47, 0x72, 0x8c, 0x17,
-	0x1e, 0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0x38, 0xe1, 0xb1, 0x1c, 0xc3, 0x85, 0xc7, 0x72, 0x0c,
-	0x37, 0x1e, 0xcb, 0x31, 0x44, 0x09, 0x43, 0x43, 0xba, 0x02, 0x12, 0xd6, 0x25, 0x95, 0x05, 0xa9,
-	0xc5, 0x49, 0x6c, 0xe0, 0xb0, 0x36, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0xae, 0xb5, 0x7b, 0x09,
-	0x87, 0x01, 0x00, 0x00,
+	// 310 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x92, 0x41, 0x4b, 0xc3, 0x30,
+	0x1c, 0xc5, 0x9b, 0x0d, 0xeb, 0xf6, 0x1f, 0xca, 0xcc, 0x86, 0xf4, 0x14, 0x34, 0x27, 0x11, 0xd6,
+	0x81, 0x7e, 0x02, 0xc7, 0x10, 0x77, 0xd1, 0x31, 0xf0, 0x22, 0x28, 0x64, 0x35, 0x8c, 0xb1, 0xd2,
+	0x84, 0xf6, 0x2f, 0xe8, 0xb7, 0xf0, 0x63, 0x79, 0xdc, 0xd1, 0xa3, 0xb4, 0x5f, 0x44, 0x9a, 0x44,
+	0x6c, 0x37, 0xe7, 0x2d, 0x7f, 0xde, 0x7b, 0x3f, 0xde, 0x83, 0x40, 0xa0, 0x63, 0x91, 0x48, 0x1c,
+	0xce, 0x63, 0xb5, 0x18, 0x6a, 0x11, 0xad, 0x24, 0x86, 0x3a, 0x55, 0xa8, 0x68, 0xc7, 0x2a, 0x61,
+	0xa9, 0xf0, 0x9c, 0xc0, 0xe1, 0x28, 0x56, 0x8b, 0xa9, 0x71, 0x8c, 0x05, 0x0a, 0x3a, 0x00, 0x3f,
+	0x51, 0xe5, 0x2b, 0x20, 0x27, 0xe4, 0xac, 0x73, 0xd1, 0x0b, 0x2b, 0x81, 0xf0, 0xd6, 0x48, 0x37,
+	0xde, 0xcc, 0x99, 0xe8, 0x35, 0x1c, 0x2c, 0xe7, 0xd1, 0x54, 0x65, 0x68, 0x19, 0x41, 0xc3, 0xa4,
+	0x58, 0x2d, 0x35, 0xa9, 0x3a, 0x1c, 0xa0, 0x1e, 0xa3, 0x77, 0xd0, 0x7d, 0xd1, 0xcf, 0x02, 0x65,
+	0x05, 0xd5, 0x34, 0xa8, 0xd3, 0x1a, 0xea, 0x7e, 0xc3, 0xe4, 0x68, 0x5b, 0xe1, 0x51, 0x0b, 0x7c,
+	0xbb, 0x9b, 0xb7, 0xc0, 0xb7, 0xb5, 0xf9, 0x23, 0x1c, 0x6d, 0x55, 0xa1, 0x7d, 0xd8, 0xc3, 0x25,
+	0xc6, 0xd2, 0xec, 0x6d, 0xcf, 0xec, 0x41, 0x03, 0xd8, 0x8f, 0x54, 0x82, 0x32, 0xb1, 0x8b, 0xda,
+	0xb3, 0x9f, 0xd3, 0x28, 0xa9, 0x14, 0xa8, 0x52, 0x53, 0xb0, 0x54, 0xec, 0xc9, 0xcf, 0xa1, 0x5b,
+	0xc3, 0x5f, 0x45, 0x2b, 0x7a, 0x0c, 0xbe, 0x56, 0x19, 0x4e, 0xc6, 0x0e, 0xef, 0x2e, 0xfe, 0x04,
+	0xfd, 0xbf, 0xa6, 0xec, 0xf2, 0xff, 0xb6, 0x6c, 0xec, 0x68, 0xd9, 0xac, 0xb5, 0xe4, 0x03, 0xe8,
+	0x6d, 0xf2, 0xff, 0xa9, 0x33, 0x1a, 0x7c, 0xe4, 0x8c, 0xac, 0x73, 0x46, 0xbe, 0x72, 0x46, 0xde,
+	0x0b, 0xe6, 0xad, 0x0b, 0xe6, 0x7d, 0x16, 0xcc, 0x7b, 0xe8, 0xb9, 0x9f, 0xf4, 0x6a, 0xff, 0x12,
+	0xbe, 0x69, 0x99, 0xcd, 0x7d, 0xf3, 0x97, 0x2e, 0xbf, 0x03, 0x00, 0x00, 0xff, 0xff, 0x1b, 0x68,
+	0x23, 0x06, 0x67, 0x02, 0x00, 0x00,
 }
 
 func (m *BlogPacketData) Marshal() (dAtA []byte, err error) {
@@ -350,6 +474,27 @@ func (m *BlogPacketData_IbcPostPacket) MarshalToSizedBuffer(dAtA []byte) (int, e
 		}
 		i--
 		dAtA[i] = 0x12
+	}
+	return len(dAtA) - i, nil
+}
+func (m *BlogPacketData_UpdatePostPacket) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *BlogPacketData_UpdatePostPacket) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	if m.UpdatePostPacket != nil {
+		{
+			size, err := m.UpdatePostPacket.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintPacket(dAtA, i, uint64(size))
+		}
+		i--
+		dAtA[i] = 0x1a
 	}
 	return len(dAtA) - i, nil
 }
@@ -450,6 +595,80 @@ func (m *IbcPostPacketAck) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *UpdatePostPacketData) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *UpdatePostPacketData) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *UpdatePostPacketData) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Content) > 0 {
+		i -= len(m.Content)
+		copy(dAtA[i:], m.Content)
+		i = encodeVarintPacket(dAtA, i, uint64(len(m.Content)))
+		i--
+		dAtA[i] = 0x1a
+	}
+	if len(m.Title) > 0 {
+		i -= len(m.Title)
+		copy(dAtA[i:], m.Title)
+		i = encodeVarintPacket(dAtA, i, uint64(len(m.Title)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.PostID) > 0 {
+		i -= len(m.PostID)
+		copy(dAtA[i:], m.PostID)
+		i = encodeVarintPacket(dAtA, i, uint64(len(m.PostID)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *UpdatePostPacketAck) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *UpdatePostPacketAck) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *UpdatePostPacketAck) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.PostID) > 0 {
+		i -= len(m.PostID)
+		copy(dAtA[i:], m.PostID)
+		i = encodeVarintPacket(dAtA, i, uint64(len(m.PostID)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintPacket(dAtA []byte, offset int, v uint64) int {
 	offset -= sovPacket(v)
 	base := offset
@@ -497,6 +716,18 @@ func (m *BlogPacketData_IbcPostPacket) Size() (n int) {
 	}
 	return n
 }
+func (m *BlogPacketData_UpdatePostPacket) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.UpdatePostPacket != nil {
+		l = m.UpdatePostPacket.Size()
+		n += 1 + l + sovPacket(uint64(l))
+	}
+	return n
+}
 func (m *NoData) Size() (n int) {
 	if m == nil {
 		return 0
@@ -528,6 +759,40 @@ func (m *IbcPostPacketData) Size() (n int) {
 }
 
 func (m *IbcPostPacketAck) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.PostID)
+	if l > 0 {
+		n += 1 + l + sovPacket(uint64(l))
+	}
+	return n
+}
+
+func (m *UpdatePostPacketData) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.PostID)
+	if l > 0 {
+		n += 1 + l + sovPacket(uint64(l))
+	}
+	l = len(m.Title)
+	if l > 0 {
+		n += 1 + l + sovPacket(uint64(l))
+	}
+	l = len(m.Content)
+	if l > 0 {
+		n += 1 + l + sovPacket(uint64(l))
+	}
+	return n
+}
+
+func (m *UpdatePostPacketAck) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -644,6 +909,41 @@ func (m *BlogPacketData) Unmarshal(dAtA []byte) error {
 				return err
 			}
 			m.Packet = &BlogPacketData_IbcPostPacket{v}
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field UpdatePostPacket", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			v := &UpdatePostPacketData{}
+			if err := v.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			m.Packet = &BlogPacketData_UpdatePostPacket{v}
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
@@ -889,6 +1189,234 @@ func (m *IbcPostPacketAck) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: IbcPostPacketAck: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PostID", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.PostID = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipPacket(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *UpdatePostPacketData) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowPacket
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: UpdatePostPacketData: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: UpdatePostPacketData: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field PostID", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.PostID = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Title", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Title = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 3:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Content", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowPacket
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthPacket
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Content = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipPacket(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthPacket
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *UpdatePostPacketAck) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowPacket
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: UpdatePostPacketAck: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: UpdatePostPacketAck: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		case 1:
